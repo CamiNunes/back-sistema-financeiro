94be2175d1902fc4db16af7d0c46e072abe62939
Camila Nunes <cvn.camila@gmail.com>
2023-10-26 21:24:46 -0300

    criação de configurações do context;

---
+++ src/SistemaFinanceiro.Infraestrutra/Configuration/ContextBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Infraestrutra.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<FinancialSystem> FinancialSystem { set; get; }
+        public DbSet<UserFinancialSystem> UserFinancialSystem { set; get; }
+        public DbSet<Category> Category { set; get; }
+        public DbSet<Expense> Expense { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnection());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string GetStringConnection()
+        {
+            return "Server=DESKTOP-IRS4H7G\\SQLEXPRESS;Database=SistemaFinanceiro;Integrated Security=False;User ID=sa;Password=8uygfe345;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            //return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=False;User ID=sa;Password=1234;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            //return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=True"; // Evitar
+        }
+    }
+}
---
+++ src/SistemaFinanceiro.Infraestrutra/Repositories/RepositoryGeneric.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using SistemaFinanceiro.Dominio.Interfaces.Generics;
+using SistemaFinanceiro.Infraestrutra.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Infraestrutra.Repositories
+{
+    public  class RepositoryGeneric<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryGeneric()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().ToListAsync();
+            }
+        }
+
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+    }
+}
